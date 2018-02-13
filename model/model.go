05ed80a04aaed34001f32ab98498eca18b6f1e0d
YAWAL <yavorskyyval@gmail.com>
2018-02-13 23:40:25 +0200

    updated model functionality

--- model/model.go
+++ model/model.go
@@ -1,22 +1,42 @@
 package model
 
-import "time"
+import (
+	"time"
+)
 
 type Tenant struct {
+	Id       int    `json:"id"`
 	Name     string `json:"name"`
 	LastName string `json:"last_name"`
 }
 
 type HotelRoom struct {
-	Number       byte `json:"number"`
-	RoomQuantity byte `json:"room_quantity"`
+	Number       int  `json:"number"`
+	RoomQuantity int  `json:"room_quantity"`
 	IsFree       bool `json:"is_free"`
 }
 
 type Rent struct {
 	Id           int       `json:"id"`
-	HotelRoomNum byte      `json:"hotel_room_num"`
-	Tenants      []Tenant  `json:"tenants"`
+	HotelRoomNum int       `json:"hotel_room_num"`
+	TenantId     int       `json:"tenant_id"`
 	StartDate    time.Time `json:"start_date"`
 	EndDate      time.Time `json:"end_date"`
+}
+
+// RentResponse represent response (list of ordered rooms with tenant's name) for rendering
+type RentResponse struct {
+	OrderedRooms map[int]string `json:"ordered_rooms"`
+}
+
+func (hr HotelRoom) GetAllHotelRooms() []HotelRoom {
+	hotelRooms := make([]HotelRoom, 0)
+	//TODO implement GetAllHotelRooms logic
+	return hotelRooms
+}
+
+func (r Rent) GetAllRents() []Rent {
+	rents := make([]Rent, 0)
+	//TODO implement GetAllRents logic
+	return rents
 }
